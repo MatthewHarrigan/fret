81d319cb4a2c05a72d19e3c4aef7eeedb6db5ac4
Matthew Harrigan <matthew.harrigan@bbc.co.uk>
2015-08-21 22:42:56 +0100

    Refactor

--- fretboard.py
+++ fretboard.py
@@ -2,18 +2,27 @@ def get_index(string, note):
     notes = ['E','F','F#/Gb','G','G#/Ab','A','A#/Bb', 'B', 'C', 'C#/Db', 'D',  'D#/Eb']
     note_i = notes.index(note)
 
-    if string == 5:
+    open_string = 0
+
+    if string == 6:
+        if note_i == 0:
+            return 12
+        else:
+            return note_i
+    elif string == 5:
+        if note_i == 5:
+            return 17 
         open_string = 5
-        if note_i < open_string:
-            return note_i + 12
+    elif string == 4:
+        if note_i == 10:
+            return 22
+        open_string = 10
+    elif string == 3:
+        if note_i == 3:
+            return 27
+        open_string = 15
 
-    # Return 12 fret for open E
-    if string == 6 and note_i == 0:
-        return 12
-    elif string == 5 and note_i == 5:
-        return 17
-    elif string == 4 and note_i == 10:
-        return 22 
+    if note_i < open_string:
+        return note_i + 12
 
-    return notes.index(note)
-
+    return note_i
--- tests.py
+++ tests.py
@@ -19,7 +19,24 @@ class GetNumberTest(unittest.TestCase):
         self.assertEqual(get_index(5, 'G#/Ab'), 16)
         self.assertEqual(get_index(5, 'A'), 17)
 
+        # Open D
         self.assertEqual(get_index(4, 'D'), 22)
+
+        self.assertEqual(get_index(4, 'D#/Eb'), 11)
+        self.assertEqual(get_index(4, 'E'), 12)
+
+        self.assertEqual(get_index(4, 'F'), 13)
+        self.assertEqual(get_index(4, 'F#/Gb'), 14)
+        self.assertEqual(get_index(4, 'G'), 15)
+        self.assertEqual(get_index(4, 'G#/Ab'), 16)
+        self.assertEqual(get_index(4, 'A'), 17)
+        self.assertEqual(get_index(4, 'A#/Bb'), 18)
+        self.assertEqual(get_index(4, 'B'), 19)
+        self.assertEqual(get_index(4, 'C'), 20)
+        self.assertEqual(get_index(4, 'C#/Db'), 21)
+
+        # Open G
+        self.assertEqual(get_index(3, 'G'), 27)
 
 if __name__ == '__main__':
     unittest.main()
