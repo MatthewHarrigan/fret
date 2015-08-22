d4a5e86a5996b3419ccbcd40dbb1184d13d4f835
Matthew Harrigan <matthew.harrigan@bbc.co.uk>
2015-08-22 23:29:01 +0100

    Draw empty table

--- display_fretboard/display_fretboard.py
+++ display_fretboard/display_fretboard.py
@@ -1,10 +1,31 @@
-def display_fretboard():
-    str = ('0 1 2 3 4 5 6 7 8 9 10 11 \n'
-           'e | | | | | | | | | | | \n'
-           'b | | | | | | | | | | | \n'
-           'g | | | | | | | | | | | \n'
-           'd | | | | | | | | | | | \n'
-           'a | | | | | | | | | | | \n'
-           'e | | | | | | | | | | | \n')
+def display_fretboard(string, note):
+
+    open_strings = ['e', 'b', 'g', 'd', 'a', 'e']
+
+    fret_numbers = '0 1 2 3 4 5 6 7 8 9 10 11 \n';
+
+    if (string == 1 and note == 'E'):
+        str = ('0 1 2 3 4 5 6 7 8 9 10 11 \n'
+               'e | | | | | | | | | | E \n'
+               'b | | | | | | | | | | | \n'
+               'g | | | | | | | | | | | \n'
+               'd | | | | | | | | | | | \n'
+               'a | | | | | | | | | | | \n'
+               'e | | | | | | | | | | | \n')
+    elif (string == 1 and note == 'F'):
+        str = ('0 1 2 3 4 5 6 7 8 9 10 11 \n'
+               'e F | | | | | | | | | | \n'
+               'b | | | | | | | | | | | \n'
+               'g | | | | | | | | | | | \n'
+               'd | | | | | | | | | | | \n'
+               'a | | | | | | | | | | | \n'
+               'e | | | | | | | | | | | \n')
+    else:
+        str = fret_numbers;
+        for open_string in open_strings:
+            str += open_string + ' '
+            for fret in range(0, 11):
+                str += '| '
+            str += '\n'
 
     return str
--- tests/test_display_fretboard.py
+++ tests/test_display_fretboard.py
@@ -1,16 +1,47 @@
 import unittest
 from display_fretboard.display_fretboard import display_fretboard
 
-class DisplayFretboardTest(unittest.TestCase):
 
+class DisplayFretboardTest(unittest.TestCase):
     def test_empty_fretboard(self):
+        string = 0
+        note = 0
 
         str = ('0 1 2 3 4 5 6 7 8 9 10 11 \n'
-            'e | | | | | | | | | | | \n'
-            'b | | | | | | | | | | | \n'
-            'g | | | | | | | | | | | \n'
-            'd | | | | | | | | | | | \n'
-            'a | | | | | | | | | | | \n'
-            'e | | | | | | | | | | | \n')
+               'e | | | | | | | | | | | \n'
+               'b | | | | | | | | | | | \n'
+               'g | | | | | | | | | | | \n'
+               'd | | | | | | | | | | | \n'
+               'a | | | | | | | | | | | \n'
+               'e | | | | | | | | | | | \n')
 
-        self.assertEqual(display_fretboard(), str)
+        self.assertEqual(display_fretboard(string, note), str)
+
+    def test_six_string_e(self):
+        string = 1
+        note = 'E'
+
+        str = ('0 1 2 3 4 5 6 7 8 9 10 11 \n'
+               'e | | | | | | | | | | E \n'
+               'b | | | | | | | | | | | \n'
+               'g | | | | | | | | | | | \n'
+               'd | | | | | | | | | | | \n'
+               'a | | | | | | | | | | | \n'
+               'e | | | | | | | | | | | \n')
+
+        self.assertEqual(display_fretboard(string, note), str)
+
+    def test_six_string_f(self):
+
+        string = 1
+        note = 'F'
+
+        str = ('0 1 2 3 4 5 6 7 8 9 10 11 \n'
+               'e F | | | | | | | | | | \n'
+               'b | | | | | | | | | | | \n'
+               'g | | | | | | | | | | | \n'
+               'd | | | | | | | | | | | \n'
+               'a | | | | | | | | | | | \n'
+               'e | | | | | | | | | | | \n')
+
+        self.assertEqual(display_fretboard(string, note), str)
