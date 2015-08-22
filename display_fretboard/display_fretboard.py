2e0d264938b56e3ca69449eabc8de982df7a3fce
Matthew Harrigan <matthew.harrigan@bbc.co.uk>
2015-08-23 00:03:34 +0100

    A couple more display tests

--- display_fretboard/display_fretboard.py
+++ display_fretboard/display_fretboard.py
@@ -1,31 +1,23 @@
-def display_fretboard(string, note):
+from fretboard.fretboard import get_index
 
+
+def display_fretboard(string, note):
     open_strings = ['e', 'b', 'g', 'd', 'a', 'e']
+    open_string_indices = [24, 19, 15, 10, 5, 0]
 
     fret_numbers = '0 1 2 3 4 5 6 7 8 9 10 11 \n';
 
-    if (string == 1 and note == 'E'):
-        str = ('0 1 2 3 4 5 6 7 8 9 10 11 \n'
-               'e | | | | | | | | | | E \n'
-               'b | | | | | | | | | | | \n'
-               'g | | | | | | | | | | | \n'
-               'd | | | | | | | | | | | \n'
-               'a | | | | | | | | | | | \n'
-               'e | | | | | | | | | | | \n')
-    elif (string == 1 and note == 'F'):
-        str = ('0 1 2 3 4 5 6 7 8 9 10 11 \n'
-               'e F | | | | | | | | | | \n'
-               'b | | | | | | | | | | | \n'
-               'g | | | | | | | | | | | \n'
-               'd | | | | | | | | | | | \n'
-               'a | | | | | | | | | | | \n'
-               'e | | | | | | | | | | | \n')
-    else:
-        str = fret_numbers;
-        for open_string in open_strings:
-            str += open_string + ' '
-            for fret in range(0, 11):
+    str = fret_numbers;
+    str_num = 1
+    for open_string in open_strings:
+        str += open_string + ' '
+        for note_index in range(open_string_indices[str_num - 1] + 1,
+                          open_string_indices[str_num - 1] + 12):
+            if str_num == string and get_index(string, note) == note_index:
+                str += note + ' '
+            else:
                 str += '| '
-            str += '\n'
+        str += '\n'
+        str_num += 1
 
     return str
--- tests/test_display_fretboard.py
+++ tests/test_display_fretboard.py
@@ -3,26 +3,12 @@ from display_fretboard.display_fretboard import display_fretboard
 
 
 class DisplayFretboardTest(unittest.TestCase):
-    def test_empty_fretboard(self):
-        string = 0
-        note = 0
-
-        str = ('0 1 2 3 4 5 6 7 8 9 10 11 \n'
-               'e | | | | | | | | | | | \n'
-               'b | | | | | | | | | | | \n'
-               'g | | | | | | | | | | | \n'
-               'd | | | | | | | | | | | \n'
-               'a | | | | | | | | | | | \n'
-               'e | | | | | | | | | | | \n')
-
-        self.assertEqual(display_fretboard(string, note), str)
-
     def test_six_string_e(self):
         string = 1
-        note = 'E'
+        note = 'D#/Eb'
 
         str = ('0 1 2 3 4 5 6 7 8 9 10 11 \n'
-               'e | | | | | | | | | | E \n'
+               'e | | | | | | | | | | D#/Eb \n'
                'b | | | | | | | | | | | \n'
                'g | | | | | | | | | | | \n'
                'd | | | | | | | | | | | \n'
@@ -32,13 +18,40 @@ class DisplayFretboardTest(unittest.TestCase):
         self.assertEqual(display_fretboard(string, note), str)
 
     def test_six_string_f(self):
-
         string = 1
         note = 'F'
 
         str = ('0 1 2 3 4 5 6 7 8 9 10 11 \n'
                'e F | | | | | | | | | | \n'
                'b | | | | | | | | | | | \n'
+               'g | | | | | | | | | | | \n'
+               'd | | | | | | | | | | | \n'
+               'a | | | | | | | | | | | \n'
+               'e | | | | | | | | | | | \n')
+
+        self.assertEqual(display_fretboard(string, note), str)
+
+    def test_fifth_string_c(self):
+        string = 1
+        note = 'F#/Gb'
+
+        str = ('0 1 2 3 4 5 6 7 8 9 10 11 \n'
+               'e | F#/Gb | | | | | | | | | \n'
+               'b | | | | | | | | | | | \n'
+               'g | | | | | | | | | | | \n'
+               'd | | | | | | | | | | | \n'
+               'a | | | | | | | | | | | \n'
+               'e | | | | | | | | | | | \n')
+
+        self.assertEqual(display_fretboard(string, note), str)
+
+    def test_fifth_string_bflat(self):
+        string = 2
+        note = 'A#/Bb'
+
+        str = ('0 1 2 3 4 5 6 7 8 9 10 11 \n'
+               'e | | | | | | | | | | | \n'
+               'b | | | | | | | | | | A#/Bb \n'
                'g | | | | | | | | | | | \n'
                'd | | | | | | | | | | | \n'
                'a | | | | | | | | | | | \n'
